d8ed450816294b2a8f4ebdb41fcc70a5cb56511a
MarshMello0 <Beny2000@hotmail.co.uk>
2019-06-10 13:17:59 +0100

    Timer in GameManager

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Session Info")]
+    [SerializeField]
+    private int score;
+    [SerializeField]
+    private float currentTime;
+
+    [Header("Settings")]
+    [SerializeField]
+    private int collectableScore;
+
+    [Header("Objects")]
+    [SerializeField]
+    private TextMeshProUGUI text;
+
+    private bool timerRunning;
+
+    private void Start()
+    {
+        currentTime = 5;
+        text.text = currentTime + "\nScore:" + score;
+        timerRunning = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (timerRunning)
+        {
+            currentTime -= Time.fixedDeltaTime;
+            if (currentTime <= 0)
+            {
+                KillPlayer();
+                return;
+            }
+            text.text = Math.Round(currentTime,2) + "\nScore:" + score;
+        }
+            
+
+    }
+
+    public void AddScore()
+    {
+        score += collectableScore;
+    }
+
+    public void KillPlayer()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
