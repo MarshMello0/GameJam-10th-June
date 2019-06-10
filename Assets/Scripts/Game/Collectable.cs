a375ed5803a132674be15b489df658ceef40dedb
MarshMello0 <Beny2000@hotmail.co.uk>
2019-06-10 13:27:16 +0100

    Collectables

---
+++ Assets/Scripts/Game/Collectable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    [SerializeField]
+    private GameManager gm;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            gm.AddScore();
+            Destroy(this.gameObject);
+        }
+    }
+}
