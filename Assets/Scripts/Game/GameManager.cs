8bc71f8537606b882db8d52dcb06da3555f399cf
MarshMello0 <Beny2000@hotmail.co.uk>
2019-06-10 14:28:53 +0100

    Generating Levels with finishes

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
 using System;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
@@ -20,8 +21,14 @@ public class GameManager : MonoBehaviour
     [Header("Objects")]
     [SerializeField]
     private TextMeshProUGUI text;
+    [SerializeField]
+    private List<Section> sections = new List<Section>();
+    [SerializeField]
+    private GameObject killPrefab, finishPrefab;
+
 
     private bool timerRunning;
+    private List<GameObject> finishes;
     [SerializeField]
     private bool disableTimer;
 
@@ -44,8 +51,6 @@ public class GameManager : MonoBehaviour
             }
             text.text = Math.Round(currentTime,2) + "\nScore:" + score;
         }
-            
-
     }
 
     public void AddScore()
@@ -57,4 +62,72 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(0,0,100,100),"Generate"))
+        {
+            GenerateLevel(0.5f, 5);
+        }
+    }
+
+    private void GenerateLevel(float chance, int maxHoles)
+    {
+        ResetLevel();
+
+        if (chance > 1)
+            chance = chance / 10f;
+
+        int amountofHoles = 0;
+        List<Section> openHoles = new List<Section>();
+
+        for (int i = 0; i < sections.Count; i++)
+        {
+            if (i > 0 && amountofHoles <= maxHoles && sections[i - 1].go.activeInHierarchy && Random.Range(0,10) >= chance * 10 ||
+                i == 0 && amountofHoles <= maxHoles && Random.Range(0, 10) >= chance * 10)
+            {
+                sections[i].go.SetActive(false);
+                amountofHoles++;
+                openHoles.Add(sections[i]);
+            }
+        }
+
+        //This is the correct whole which has the finish flag
+        int correctHole = Random.Range(0, openHoles.Count - 1);
+        finishes = new List<GameObject>(openHoles.Count);
+
+        for (int i = 0; i < openHoles.Count; i++)
+        {
+            GameObject bottom = Instantiate(i == correctHole ? finishPrefab : killPrefab);
+            bottom.transform.position = openHoles[i].bottomPoint.position;
+            bottom.AddComponent<ObjectCollision>().isFinish = i == correctHole;
+            finishes.Add(bottom);
+        }
+        
+    }
+
+    private void ResetLevel()
+    {
+        foreach (Section section in sections)
+        {
+            section.go.SetActive(true);
+        }
+
+
+        if (finishes != null)
+        {
+            for (int i = 0; i < finishes.Count; i++)
+            {
+                Destroy(finishes[i]);
+            }
+        }
+
+    }
+}
+[Serializable]
+public class Section
+{
+    public GameObject go;
+    public BoxCollider2D col;
+    public Transform bottomPoint;
 }
---
+++ Assets/Scripts/Game/ObjectCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCollision : MonoBehaviour
+{
+    public bool isFinish;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Debug.Log("Hit player, isFinished: " + isFinish);
+        }
+    }
+
+}
