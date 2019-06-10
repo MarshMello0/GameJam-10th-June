0f2a3aaa6a5e22a0f5c033ad25f10ca6e6a87a10
MarshMello0 <Beny2000@hotmail.co.uk>
2019-06-10 11:49:17 +0100

    Setting Up Scenes

---
+++ Assets/Scripts/Utility/Button.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Button : MonoBehaviour, IPointerClickHandler
+{
+    public EventTrigger.TriggerEvent callBack;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        callBack.Invoke(eventData);
+    }
+}
